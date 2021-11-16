5b980c48c212228fc85468c6aecfbf2e6b213f1d
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2021-11-15 22:25:57 -0500

    Update generator_session.m

--- project 4/generator_session.m
+++ project 4/generator_session.m
@@ -7,6 +7,9 @@ format long;
 rho = 1225; %g/m^3
 D = 0.13208; % meters
 A = (pi/4)*D^2;
+resistance = 120; % ohms
+a = (1/3);
+
 
 % importing data
 air_flow = dlmread("air_flow.txt");
@@ -21,6 +24,9 @@ printf("     -----------------     |    --------------       |    --------------
 % power calc
 i = 1;
 for i = 1:n
-gross_power(i) = 0.5*rho*A*(air_flow(i)^3)*exp(1);
-printf('         %.2f             |        %.2f             |        %.4f\n',air_flow(i),rms(i),gross_power(i))
+  power(i) = 2*rho*A*(air_flow(i)^3)*a*(1-a)^2;
+  gross_power(i) = 0.5*rho*A*(air_flow(i)^3)*exp(1)*.001; %watts
+  coeff_power(i) = power(i)/gross_power(i);
+  printf('         %.2f             |        %.2f             |        %.4f\n',air_flow(i),rms(i),gross_power(i))
 endfor
+
