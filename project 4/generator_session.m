0eaa8ad8910c00e115390bb49485487af0b3895f
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2021-11-15 23:42:01 -0500

    Update generator_session.m

--- project 4/generator_session.m
+++ project 4/generator_session.m
@@ -24,9 +24,27 @@ printf("     -----------------     |    --------------       |    --------------
 % power calc
 i = 1;
 for i = 1:n
-  power(i) = 2*rho*A*(air_flow(i)^3)*a*(1-a)^2;
+  power(i) = 2*rho*A*(air_flow(i)^3)*a*(1-a)^2*.001;
   gross_power(i) = 0.5*rho*A*(air_flow(i)^3)*exp(1)*.001; %watts
   coeff_power(i) = power(i)/gross_power(i);
   printf('         %.2f             |        %.2f             |        %.4f\n',air_flow(i),rms(i),gross_power(i))
 endfor
 
+x = air_flow;
+for i = 1:n
+y1(i) = 4.97*10^-03*x(i)^3;
+y2(i) = 350 + -31.3*x(i) + 0.738*x(i)^2;
+endfor
+
+figure(1)
+hold on
+grid minor on
+plot(air_flow,power,'ro','linewidth',3)
+plot(x,y1)
+plot(x,y2)
+title("Power as a Function of Air Velocity")
+xlabel('Power (W)')
+ylabel('Air Flow (m/s)')
+legend('Power','4.97*10^-03*x^3','350 + -31.3*x + 0.738*x^2')
+hold off
+##print -dpng figure1gen.png
